c8cba9e2062edff6b4c75496601d59d7fa79ea76
Matthew Browne <matthew.browne@retailalchemy.co.uk>
2024-02-08 16:26:04 +0000

    requirements file

--- Starter.py
+++ Starter.py
@@ -809,6 +809,6 @@ if __name__ == '__main__':
      with app.app_context():
 
         db.create_all()
-        for user in user_data:
-            add_user(user)
+        # for user in user_data:
+        #     add_user(user)
         app.run(host="0.0.0.0", debug=True)
---
+++ misc.py
@@ -0,0 +1,17 @@
+import subprocess
+
+def generate_requirements_file(output_file='requirements.txt'):
+    # Run 'pip freeze' to get a list of installed packages and their versions
+    result = subprocess.run(['pip', 'freeze'], capture_output=True, text=True)
+    
+    # Check if the command was successful
+    if result.returncode == 0:
+        # Write the output to the specified file
+        with open(output_file, 'w') as file:
+            file.write(result.stdout)
+        print(f"Requirements file '{output_file}' generated successfully.")
+    else:
+        print("Failed to generate the requirements file.")
+
+# Call the function to generate the requirements.txt file
+generate_requirements_file()
