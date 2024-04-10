d91a73b462365b46a7154cc6d19e92863ef653f4
Matthew Browne <matthew.browne@retailalchemy.co.uk>
2024-04-10 15:32:12 +0100

    added .env

--- app.py
+++ app.py
@@ -25,6 +25,8 @@ import queue
 import pyotp
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
+import app_config
+from identity.flask import Auth
 
 #from azure import identity
 
@@ -45,6 +47,20 @@ azure_database = "postgres"
 ra_server_uri = 'postgresql://postgres:' + urllib.parse.quote_plus("Gde3400@@") + '@192.168.1.2:5432/CPW Blueprint'
 
 # Create the new PostgreSQL URI for Azure
+app.config.from_object(app_config)
+
+auth = Auth(
+    app,
+    authority=os.getenv("AUTHORITY"),
+    client_id=os.getenv("CLIENT_ID"),
+    client_credential=os.getenv("CLIENT_SECRET"),
+    redirect_uri=os.getenv("REDIRECT_URI"),
+    oidc_authority=os.getenv("OIDC_AUTHORITY"),
+    b2c_tenant_name=os.getenv('B2C_TENANT_NAME'),
+    b2c_signup_signin_user_flow=os.getenv('SIGNUPSIGNIN_USER_FLOW'),
+    b2c_edit_profile_user_flow=os.getenv('EDITPROFILE_USER_FLOW'),
+    b2c_reset_password_user_flow=os.getenv('RESETPASSWORD_USER_FLOW'),
+)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = ra_server_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
