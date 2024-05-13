f5e3c591abe3e65ad2291ec2cd54a616e5701b69
Matthew Browne <matthew.browne@retailalchemy.co.uk>
2024-05-13 15:04:44 +0100

    amended password reset config

--- app_config.py
+++ app_config.py
@@ -28,17 +28,12 @@ b2c_tenant = "acblueprint"
 signupsignin_user_flow = "B2C_1_BP_STANDARD_USER_SIGN_IN"
 editprofile_user_flow = "B2C_1_profileediting1"
 
-resetpassword_user_flow = "B2C_1_passwordreset1"  # Note: Legacy setting.
-
 authority_template = "{authority}/{user_flow}"
 
 AUTHORITY = authority_template.format(
     authority=authority, user_flow=signupsignin_user_flow)
 B2C_PROFILE_AUTHORITY = authority_template.format(
     authority=authority, user_flow=editprofile_user_flow)
-
-B2C_RESET_PASSWORD_AUTHORITY = authority_template.format(
-    authority=authority, user_flow=resetpassword_user_flow)
 
 REDIRECT_PATH = "/auth/redirect"
 
